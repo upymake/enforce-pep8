8c5fc1f0373c2531f25c49f04384ca18ae135590
Volodymyr Yahello <vyahello@gmail.com>
2020-04-02 16:35:48 +0300

    Release 0.0.1 version

--- punish/__init__.py
+++ punish/__init__.py
@@ -8,4 +8,4 @@ Please check for https://www.python.org/dev/peps/pep-0008/ to get familiar with 
 
 __author__: str = "Volodymyr Yahello"
 __email__: str = "vyahello@gmail.com"
-__version__: str = "0.0.0"
+__version__: str = "0.0.1"
