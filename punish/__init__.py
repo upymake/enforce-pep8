d415c6607c9a6cd165284a22b4f8a9d51f08c9ec
Volodymyr Yahello <vyahello@gmail.com>
2020-04-16 23:33:43 +0300

    Release 0.0.6 version

--- punish/__init__.py
+++ punish/__init__.py
@@ -9,4 +9,4 @@ Please check for https://www.python.org/dev/peps/pep-0008/ to get familiar with 
 __author__: str = "Volodymyr Yahello"
 __email__: str = "vyahello@gmail.com"
 __package_name__: str = "enforce-pep8"
-__version__: str = "0.0.5"
+__version__: str = "0.0.6"
--- punish/type.py
+++ punish/type.py
@@ -7,7 +7,7 @@ from functools import wraps
 from inspect import Signature, signature
 from types import TracebackType
 from typing import Any, Callable, ContextManager, Dict, List, Optional, Tuple, Type
-from punish.style import AbstractStyle, abstractstyle
+from punish.style import abstractstyle
 
 
 class Typed:
@@ -216,7 +216,7 @@ def typed_property(name: str, expected_type: Type[Any]) -> property:
     return prop  # type: ignore
 
 
-class AbstractContextManager(AbstractStyle, ContextManager["AbstractContextManager"]):
+class AbstractContextManager(ContextManager["AbstractContextManager"]):
     """An interface describing an abstract friendly context manager connection.
 
     Example:
@@ -236,8 +236,7 @@ class AbstractContextManager(AbstractStyle, ContextManager["AbstractContextManag
         ...    ) -> None:
         ...        self._db.pop(1)
         ...
-        >>> with Database([10]) as db:
-        ...     ...
+        >>>
     """
 
     @abstractstyle
